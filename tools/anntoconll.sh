f60ac19c013a1f7d761a48625c461f354d691f99
Oyesh Mann Singh <oyeshsin@hotmail.com>
2020-02-10 19:39:19 -0500

    Modified to include all the files in subdirectories at once

--- tools/anntoconll.sh
+++ tools/anntoconll.sh
@@ -4,12 +4,20 @@
 # How to run
 # bash anntoconll.sh ../data/nepsa/Avenues_Khabar/0S8tX4eRa6M/
 
+# Reference : https://stackoverflow.com/questions/9612090/how-to-loop-through-file-names-returned-by-find
+
 # Input data dir
 DATA_DIR=$1
 
 # For all *.txt files
 # run the script
-for filename in $DATA_DIR/*.txt; do
-	echo "Processing: $filename"
-	python3 anntoconll.py $filename
+
+#find $DATA_DIR -iname '*.txt' -exec echo "Processing {}" \;
+
+# Make sure globstar is enabled
+shopt -s globstar
+for annfile in $DATA_DIR**/*.ann; do # Whitespace-safe and recursive
+	txtfile=${annfile%.*}.txt
+	echo "Processing: $txtfile"
+	python3 anntoconll.py $txtfile
 done
